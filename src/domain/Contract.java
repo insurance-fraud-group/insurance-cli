859b22a94d7834c44ea393d7c0f4790e057ee05c
jongwooo <han980817@gmail.com>
2022-05-10 17:50:07 +0900

    feat: Create getters and setters

--- src/domain/Contract.java
+++ src/domain/Contract.java
@@ -14,4 +14,44 @@ public class Contract {
   public Contract() {
 
   }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public boolean isCompletion() {
+    return completion;
+  }
+
+  public void setCompletion(boolean completion) {
+    this.completion = completion;
+  }
+
+  public Customer getCustomer() {
+    return customer;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+
+  public File getSubscription() {
+    return subscription;
+  }
+
+  public void setSubscription(File subscription) {
+    this.subscription = subscription;
+  }
 }
--- src/domain/Underwriting.java
+++ src/domain/Underwriting.java
@@ -15,4 +15,60 @@ public class Underwriting {
   public Underwriting() {
 
   }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public Customer getCustomer() {
+    return customer;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+
+  public int getEnvironmentalFactorScore() {
+    return environmentalFactorScore;
+  }
+
+  public void setEnvironmentalFactorScore(int environmentalFactorScore) {
+    this.environmentalFactorScore = environmentalFactorScore;
+  }
+
+  public int getFinancialFactorScore() {
+    return financialFactorScore;
+  }
+
+  public void setFinancialFactorScore(int financialFactorScore) {
+    this.financialFactorScore = financialFactorScore;
+  }
+
+  public int getMoralFactorScore() {
+    return moralFactorScore;
+  }
+
+  public void setMoralFactorScore(int moralFactorScore) {
+    this.moralFactorScore = moralFactorScore;
+  }
+
+  public int getPhysicalFactorScore() {
+    return physicalFactorScore;
+  }
+
+  public void setPhysicalFactorScore(int physicalFactorScore) {
+    this.physicalFactorScore = physicalFactorScore;
+  }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
 }
