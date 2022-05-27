8b8c062446c6f7e410916162f62bd820ecf2adbf
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-05-27 22:29:19 +0900

    feat: Replace db connection logic related auth to repository

--- src/main/java/repository/EmployeeRepository.java
+++ src/main/java/repository/EmployeeRepository.java
@@ -1,5 +1,27 @@
 package repository;
 
+import domain.Employee;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import utils.Hibernate;
+
 public class EmployeeRepository {
 
+  public void save(Employee employee) {
+    Session session = Hibernate.getSessionFactory().openSession();
+    Transaction tx = session.beginTransaction();
+    session.save(employee);
+    tx.commit();
+  }
+
+  public Employee findByEmail(String email) {
+    Session session = Hibernate.getSessionFactory().openSession();
+    Transaction tx = session.beginTransaction();
+    Query query = session.createQuery("from Employee where email =:email ")
+        .setParameter("email", email);
+    Employee employee = (Employee) query.uniqueResult();
+    tx.commit();
+    return employee;
+  }
 }
