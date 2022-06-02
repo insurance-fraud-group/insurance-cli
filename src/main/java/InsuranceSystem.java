bfc8a74fd5f6554ff36b18b008e358568e15646f
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-06-03 05:15:18 +0900

    chore: Set off hibernate log

--- src/main/java/InsuranceSystem.java
+++ src/main/java/InsuranceSystem.java
@@ -1,8 +1,13 @@
 import cmd.AuthCommand;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class InsuranceSystem {
 
   public static void main(String[] args) {
+    Logger log = Logger.getLogger("org.hibernate");
+    log.setLevel(Level.OFF);
+
     AuthCommand.run();
   }
 }
