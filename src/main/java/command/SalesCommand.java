2eb894de86e2fa391d4e7a7f456e070310d10bfe
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-06-04 13:20:24 +0900

    fix: Change print method in sales command

--- src/main/java/command/SalesCommand.java
+++ src/main/java/command/SalesCommand.java
@@ -2,19 +2,27 @@ package command;
 
 import command.menu.sales.Sales;
 import command.menu.sales.SalesManagement;
+import domain.Insurance;
+import java.util.List;
+import service.impl.SalesServiceImpl;
 
 public class SalesCommand extends Command {
 
+  private static final SalesServiceImpl salesService = new SalesServiceImpl();
+
   public static void run() {
-    print("[영업]", Sales.values());
+    printMenu("영업", Sales.values());
   }
 
   public static void manageSales() {
-    print("[영업 관리]", SalesManagement.values());
+    printMenu("영업 관리", SalesManagement.values());
   }
 
   public static void manageInsurance() {
+    printTitle("보험 관리");
+    List<Insurance> insuranceList = salesService.getInsuranceList();
 
+    Command.printTable(insuranceList);
   }
 
   public static void manageCustomer() {
