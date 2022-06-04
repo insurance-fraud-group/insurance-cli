a003da27765caf5f371d7c657a86ad466529d453
parkcoldroad <coldstreet@naver.com>
2022-06-05 03:47:05 +0900

    feat: Create manageLossRate function

--- src/main/java/command/UnderwritingCommand.java
+++ src/main/java/command/UnderwritingCommand.java
@@ -3,8 +3,14 @@ package command;
 import command.menu.underwriting.UWManagement;
 import command.menu.underwriting.UnderwritingMenu;
 import command.parser.UnderwritingParser;
+import domain.Employee;
+import domain.Insurance;
 import domain.Underwriting;
+import domain.enums.InsuranceType;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import service.impl.UnderwritingServiceImpl;
 
 public class UnderwritingCommand extends Command {
@@ -43,6 +49,25 @@ public class UnderwritingCommand extends Command {
     printTable(data);
   }
 
+  public static void manageLossRate() {
+    printTitle("손해율 관리");
+    System.out.println("현재 등록된 보험 리스트");
+    List<Insurance> insuranceList = underwritingImpl.getInsuranceist();
+    printTable(insuranceList);
+
+    InsuranceType insuranceType = underwritingParser.getInsuranceType();
+    List<Insurance> filteredList = insuranceList.stream()
+        .filter(i -> i.getInsuranceType().name().equals(insuranceType.name()))
+        .collect(Collectors.toList());
+    printTable(filteredList);
+
+    System.out.println("관리할 상품을 선택해주세요");
+    int selectedMenu = input();
+    System.out.println("선택한 상품의 요율은 아래와 같습니다");
+    System.out.println((insuranceList.get(selectedMenu).getPremiumRate()));
+
+  }
+
   public static void underwrite() {
 
     printMenu("인수심사", UWManagement.values());
@@ -53,10 +78,6 @@ public class UnderwritingCommand extends Command {
   }
 
   public static void manageReinsurance() {
-
-  }
-
-  public static void manageLossRate() {
 
   }
 }
