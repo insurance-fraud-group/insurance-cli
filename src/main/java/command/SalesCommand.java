200737402f1d01126e64706ff9c9247ba6554f94
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-06-05 13:44:29 +0900

    refactor: Add missing messages

--- src/main/java/command/SalesCommand.java
+++ src/main/java/command/SalesCommand.java
@@ -26,14 +26,18 @@ public class SalesCommand extends Command {
   }
 
   public static void manageInsurance() {
+    printTitle("보험 관리");
+    System.out.println("상세 조회할 보험을 선택해주세요.");
     String[] args = {"insuranceCode", "name", "insuranceType", "coverDescription"};
     List<Insurance> insuranceList = salesService.getInsuranceList();
-    printTitle("보험 관리");
     printTable(insuranceList, args);
 
     Insurance insurance = insuranceList.get(input());
-    printTitle("보험 정보 조회");
+    printTitle("보험 상세 정보");
     printTable(insurance);
+    System.out.println("보험 정보 조회를 완료하였습니다.");
+
+    AuthCommand.initialize();
   }
 
   public static void manageCustomer() {
@@ -42,18 +46,22 @@ public class SalesCommand extends Command {
 
   public static void searchCustomer() {
     printTitle("고객 조회");
+    System.out.println("상세 조회할 고객을 선택해주세요.");
     String[] args = {"name", "birth", "sex"};
     List<Customer> customerList = salesService.getCustomerList();
     printTable(customerList, args);
 
     Customer customer = customerList.get(input());
-
     printTitle("고객 정보 상세 조회");
     printTable(customer);
+    System.out.println("고객 정보 조회를 완료하였습니다.");
+
+    AuthCommand.initialize();
   }
 
   public static void signUpCustomer() {
     printTitle("신규 고객");
+    System.out.println("신규 고객 정보를 입력해주세요.");
     Customer customer = Customer.builder()
         .accountNumber(parser.getAccountNumber())
         .address(parser.getAddress())
@@ -65,6 +73,9 @@ public class SalesCommand extends Command {
         .build();
 
     salesService.createCustomer(customer);
+    System.out.println("신규 고객 가입이 완료되었습니다.");
+
+    AuthCommand.initialize();
   }
 
   public static void manageOrganization() {
@@ -77,6 +88,9 @@ public class SalesCommand extends Command {
     String[] args = {"id", "name", "email", "employeeType"};
     printTitle("사원 인적 정보");
     printTable(employee, args);
+    System.out.println("사원 정보 조회를 완료하였습니다.");
+
+    AuthCommand.initialize();
   }
 
   public static void manageContract() {
@@ -85,15 +99,22 @@ public class SalesCommand extends Command {
 
   public static void searchAllContract() {
     printTitle("모든 계약 조회");
+    System.out.println("상세 정보를 조회할 계약을 선택해주세요.");
     String[] args = {"applicationDate", "customer", "insurance"};
     List<Contract> contractList = salesService.getContractList();
     printTable(contractList, args);
+
     Contract contract = contractList.get(input());
+    printTitle("계약 상세 정보");
     printTable(contract);
+    System.out.println("계약 정보 조회를 완료하였습니다.");
+
+    AuthCommand.initialize();
   }
 
   public static void searchUnsignedContract() {
     printTitle("미체결 계약 리스트");
+    System.out.println("상세 정보를 조회할 계약을 선택해주세요.");
     String[] args = {"applicationDate", "customer", "insurance"};
     List<Contract> unsignedContractList = salesService.getUnsignedContractList();
     printTable(unsignedContractList, args);
@@ -101,11 +122,15 @@ public class SalesCommand extends Command {
     Contract selectedContract = unsignedContractList.get(input());
     printTable(selectedContract);
     concludeContract(selectedContract);
+    System.out.println("미체결 정보 조회를 완료하였습니다.");
+
+    AuthCommand.initialize();
   }
 
   public static void concludeContract(Contract contract) {
 
   }
+
 
   public static void makeContract() {
 
