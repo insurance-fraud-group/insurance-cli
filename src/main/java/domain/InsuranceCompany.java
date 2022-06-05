af8282ac8fec219b8bb7d2699112a197990a1864
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-06-06 03:48:47 +0900

    feat: Add insurance company domain

---
+++ src/main/java/domain/InsuranceCompany.java
@@ -0,0 +1,24 @@
+package domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class InsuranceCompany {
+
+  @Id
+  private int id;
+
+  private String name;
+  private int shareRate;
+}
