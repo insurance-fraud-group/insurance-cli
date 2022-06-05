2dc8f29771ff7cbab5a149e034f5d9dc4a560e83
parkcoldroad <coldstreet@naver.com>
2022-06-06 03:56:50 +0900

    feat: Create classes about AccidentInvesigator

---
+++ src/main/java/service/impl/AccidentInvestigatorServiceImpl.java
@@ -0,0 +1,16 @@
+package service.impl;
+
+import service.AccidentInvestigatorService;
+
+public class AccidentInvestigatorServiceImpl implements AccidentInvestigatorService {
+
+  @Override
+  public void receiveAccidentDispatch() {
+
+  }
+
+  @Override
+  public void processAccident() {
+
+  }
+}
